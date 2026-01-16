acc4cb6dc1fc2e07c062c86aa1e50fba33f475aa
Phasawut Imsurman <68010867@kmitl.ac.th>
2026-01-16 11:16:46 +0700

    a8

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,33 @@
-export function getPostsByUser() {}
+
+import axios from 'axios';
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface UserPost {
+  id: number;
+  title: string;
+}
+
+export async function getPostsByUser(userId: number): Promise<UserPost[]> {
+  const url = 'https://jsonplaceholder.typicode.com/posts';
+
+  try {
+    const { data } = await axios.get<Post[]>(url);
+
+   return data
+      .filter((post) => post.userId === userId)
+      .map(({ id, title }) => ({
+        id,
+        title,
+      }));
+      
+  } catch (error) {
+   
+    throw new Error(`Could not fetch posts for user: ${userId}`);
+  }
+}
